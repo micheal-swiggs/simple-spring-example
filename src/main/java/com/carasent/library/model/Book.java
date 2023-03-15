20696d6b6d8841be688ba0f212902f9e4469ad78
micheal swiggs-malmevik <mail@micheal.in>
2023-03-15 23:13:22 +0100

    Book: Adds fields `borrowedAt` and `dueBackBy` & db support

--- src/main/java/com/carasent/library/model/Book.java
+++ src/main/java/com/carasent/library/model/Book.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Entity
 @Table(name="books")
@@ -28,5 +30,11 @@ public class Book {
     @NotNull
     @Column(name = "condition", nullable = false)
     private String condition;
+
+    @Column(name = "borrowed_at")
+    Date borrowedAt;
+
+    @Column(name = "due_back_by")
+    Date dueBackBy;
 
 }
--- src/main/resources/db/changelog/book-setup.sql
+++ src/main/resources/db/changelog/book-setup.sql
@@ -3,5 +3,7 @@ CREATE TABLE books (
     title               VARCHAR(200)    NOT NULL,
     author              VARCHAR(200)    NOT NULL,
     year_of_purchase    SMALLINT        NOT NULL,
-    condition           VARCHAR(50)
+    condition           VARCHAR(50),
+    borrowed_at         DATE,
+    due_back_by         DATE
 );
