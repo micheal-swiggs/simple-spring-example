398f01714fd24223cfa46e250d5cb9ec11e0916a
micheal swiggs-malmevik <mail@micheal.in>
2023-03-15 21:36:19 +0100

    BookController: Adds `BookRepository` and saves new `Book`s to db

--- src/main/java/com/carasent/library/controller/BookController.java
+++ src/main/java/com/carasent/library/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.carasent.library.controller;
 
 import com.carasent.library.model.Book;
+import com.carasent.library.repository.BookRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("books")
 public class BookController {
 
+    @Autowired
+    BookRepository bookRepository;
+
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@Valid @RequestBody Book newBook){
-        return newBook;
+        return bookRepository.save(newBook);
     }
 }
