76774863c5b1e5cb59c6383a387f023cbaef4e38
micheal swiggs-malmevik <mail@micheal.in>
2023-04-24 20:42:03 +0200

    BookController: updates controller to use relevant dtos

--- src/main/java/com/carasent/library/web/controller/BookController.java
+++ src/main/java/com/carasent/library/web/controller/BookController.java
@@ -3,6 +3,9 @@ package com.carasent.library.web.controller;
 import com.carasent.library.model.Book;
 import com.carasent.library.service.BookService;
 import com.carasent.library.web.dto.BookPartialUpdate;
+import com.carasent.library.web.dto.BookRequest;
+import com.carasent.library.web.dto.BookResponse;
+import com.carasent.library.web.dto.PageResponse;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import static com.carasent.library.web.dto.BookMapper.mapToEntity;
+import static com.carasent.library.web.dto.BookMapper.mapToResponse;
+import static com.carasent.library.web.dto.PageMapper.mapToResponse;
 
 
 @RestController
@@ -22,33 +29,37 @@ public class BookController {
     @RolesAllowed({"ADMIN"})
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Book createBook(@Valid @RequestBody Book newBook) {
-        return bookService.save(newBook);
+    public BookResponse createBook(@Valid @RequestBody BookRequest newBook) {
+        Book book = bookService.save(mapToEntity(newBook));
+        return mapToResponse(book);
     }
 
     @RolesAllowed({"ADMIN", "USER"})
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public Page<Book> getBooks(@RequestParam(name = "page", defaultValue = "0") int pageNumber,
-                               @RequestParam(name = "size", defaultValue = "10") int pageSize) {
-        return bookService.findAll(Pageable.ofSize(pageSize).withPage(pageNumber));
+    public PageResponse<BookResponse> getBooks(@RequestParam(name = "page", defaultValue = "0") int pageNumber,
+                                               @RequestParam(name = "size", defaultValue = "10") int pageSize) {
+        Page<Book> pageBook = bookService.findAll(Pageable.ofSize(pageSize).withPage(pageNumber));
+        return mapToResponse(pageBook);
     }
 
     @RolesAllowed({"ADMIN", "USER"})
     @PatchMapping("/{book-id}")
     @ResponseStatus(HttpStatus.OK)
-    public Book patchBook(@PathVariable("book-id") Integer bookId,
-                          @RequestBody BookPartialUpdate patchUpdate) {
+    public BookResponse patchBook(@PathVariable("book-id") Integer bookId,
+                                  @RequestBody BookPartialUpdate patchUpdate) {
 
-        return bookService.partiallyUpdateBook(bookId, patchUpdate);
+        Book book = bookService.partiallyUpdateBook(bookId, patchUpdate);
+        return mapToResponse(book);
     }
 
     @RolesAllowed({"ADMIN", "USER"})
     @GetMapping("/overdue")
     @ResponseStatus(HttpStatus.OK)
-    public Page<Book> getOverdueBooks(@RequestParam(name = "page", defaultValue = "0") int pageNumber,
-                                      @RequestParam(name = "size", defaultValue = "10") int pageSize) {
+    public PageResponse<BookResponse> getOverdueBooks(@RequestParam(name = "page", defaultValue = "0") int pageNumber,
+                                                      @RequestParam(name = "size", defaultValue = "10") int pageSize) {
         Pageable pageable = Pageable.ofSize(pageSize).withPage(pageNumber);
-        return bookService.findOverdueBooks(pageable);
+        Page<Book> overdueBooks = bookService.findOverdueBooks(pageable);
+        return mapToResponse(overdueBooks);
     }
 }
